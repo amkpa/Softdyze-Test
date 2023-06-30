471804f42dc4fb90c1a9fa7e88da796945005c4c
amkpa <amitkumarpalai3@gmail.com>
2023-06-30 15:45:52 +0530

    Change 03

--- src/main/java/com/softfdyze/entity/SoftDyzeData.java
+++ src/main/java/com/softfdyze/entity/SoftDyzeData.java
@@ -5,13 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.Data;
 
 @Data
 @Entity
 public class SoftDyzeData {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
+	@GenericGenerator(name="native",strategy = "native")
 	private Integer id;
 	private String name;
 	private String email;
